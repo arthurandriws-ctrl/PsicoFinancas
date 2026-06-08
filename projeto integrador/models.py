4af76faf0513dfdb28f64502eec016d9ebaa977f
Arthur <260494213+arthurandriws-ctrl@users.noreply.github.com>
2026-06-08 14:34:32 -0300

    Add files via upload

---
+++ projeto integrador/models.py
@@ -0,0 +1,8 @@
+from db import db
+
+class Usuario(db.Model):
+    __tablename__ = 'usuarios'
+    id = db.Column(db.Integer, primary_key = True,)
+    nome = db.Column(db.String(50),nullable = False,unique = True )
+    email = db.Column(db.String(50), nullable = False, unique = True)
+    senha = db.Column(db.String(255),nullable = False,)
